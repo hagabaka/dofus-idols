ed9c6edd729cf39a4eeb26187005974662dbacd9
Yaohan Chen <yaohan.chen@gmail.com>
2015-07-11 16:07:50 -0400

    Switch to text input to allow pasting

--- setupSelectize.js
+++ setupSelectize.js
@@ -20,10 +20,15 @@ Selectize.define('only_close_when_full', function() {
 var $combinationEntry = $('#combination-entry');
 $combinationEntry.selectize({
   maxItems: 6,
-  plugins: ['only_close_when_full', 'remove_button'],
+  options: allIdols,
+  valueField: 'name',
+  labelField: 'name',
+  searchField: ['name'],
   sortField: [{field: 'score', direction: 'desc'},
               {field: 'name'},
               {field: 'group'},
               {field: 'ineligible'},
               {field: 'level'}],
+  plugins: ['only_close_when_full', 'remove_button'],
+  selectOnTab: true,
 });
--- viewModel.js
+++ viewModel.js
@@ -1,14 +1,21 @@
 var ViewModel = function() {
   var self = this;
   this.visibleIdols = ko.observableArray(allIdols);
-  this.combinationEntry = ko.observableArray(new Array(6));
+  this.combinationEntry = ko.observable('');
+  this.combinationIdols = ko.computed(function() {
+    var entry = self.combinationEntry();
+    if(entry === '') {
+      return [];
+    } else {
+      return entry.split(',').map(function(name) {
+        return idolNamed[name];
+      });
+    }
+  });
   this.synergies = ko.observableArray([]);
   this.score = ko.computed(function() {
     self.synergies([]);
-    var idols = self.combinationEntry().map(function(name) {
-      return idolNamed[name];
-    });
-    return totalScore(idols, function(synergy) {
+    return totalScore(self.combinationIdols(), function(synergy) {
       self.synergies.push(synergy);
     });
   });
