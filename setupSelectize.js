03b4793467f8c771e2c705a81d8f89e885c8ba5b
Yaohan Chen <yaohan.chen@gmail.com>
2015-07-11 00:54:15 -0400

    Reformat js and remove unused code

--- setupSelectize.js
+++ setupSelectize.js
@@ -1,27 +1,25 @@
-      Selectize.define('only_close_when_full', function() {
-        var self = this;
+Selectize.define('only_close_when_full', function() {
+  var self = this;
 
-        this.on('initialize', function() {
-          self.open();
-        });
+  this.on('initialize', function() {
+    self.open();
+  });
 
-        this.close = (function() {
-          var originalClose = self.close;
-          return function() {
-            if(self.isFull()) {
-              return originalClose.apply(this, arguments);
-            } else {
-              return self;
-            }
-          };
-        })();
-      });
+  this.close = (function() {
+    var originalClose = self.close;
+    return function() {
+      if(self.isFull()) {
+        return originalClose.apply(this, arguments);
+      } else {
+        return self;
+      }
+    };
+  })();
+});
 
-      var $combinationEntry = $('#combination-entry');
-      $combinationEntry.selectize({
-        maxItems: 6,
-        plugins: ['only_close_when_full', 'remove_button'],
-      });
-      $combinationEntry.on('dropdown_close', function() {
-        $combinationEntry.trigger('dropdown_open');
-      });
+var $combinationEntry = $('#combination-entry');
+$combinationEntry.selectize({
+  maxItems: 6,
+  plugins: ['only_close_when_full', 'remove_button'],
+});
+
