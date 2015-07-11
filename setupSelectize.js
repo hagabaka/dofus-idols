01876248b8db346c3bfe441d35884f2ead25fe48
Yaohan Chen <yaohan.chen@gmail.com>
2015-07-11 02:17:45 -0400

    Sort idol choices

--- setupSelectize.js
+++ setupSelectize.js
@@ -21,5 +21,9 @@ var $combinationEntry = $('#combination-entry');
 $combinationEntry.selectize({
   maxItems: 6,
   plugins: ['only_close_when_full', 'remove_button'],
+  sortField: [{field: 'score', direction: 'desc'},
+              {field: 'name'},
+              {field: 'group'},
+              {field: 'ineligible'},
+              {field: 'level'}],
 });
-
