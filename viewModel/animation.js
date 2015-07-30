6e3b7c6aa580cd2974823f4489e0ab1604aa0d4d
Yaohan Chen <yaohan.chen@gmail.com>
2015-07-30 14:51:41 -0400

    Add animation on moving idols

--- viewModel/animation.js
+++ viewModel/animation.js
@@ -1,55 +1,70 @@
 define(['move'], function(move) {
   return function(model, ko, $) {
     model.idols.forEach(function(idol) {
-      idol.elementInMenu = ko.observable();
-      idol.elementInCombination = ko.observable();
-      idol.moveDirection = ko.observable();
-      idol.moveVector = ko.pureComputed(function() {
-        if(! (idol.moveDirection() && idol.elementInCombination() && idol.elementInMenu()) ) {
-          return null;
-        }
-        var vector = {idol: idol};
-        if(idol.moveDirection() === 'Menu') {
-          vector.origin = idol.elementInCombination();
-          vector.target = idol.elementInMenu();
-        } else {
-          vector.origin = idol.elementInMenu();
-          vector.target = idol.elementInCombination();
-        }
-        return vector;
+      idol.moveFrom = ko.observable();
+      idol.moveTo = ko.observable();
+
+      idol.readyToMove = ko.pureComputed(function() {
+        return idol.moveFrom() && idol.moveTo();
       });
-      idol.moveVector.subscribe(function(vector) {
-        if(vector) {
-          console.log('playing animation', vector);
-          var destination = $(vector.target).position();
-          move(vector.origin).to(destination.x, destination.y);
-          idol.elementInMenu(null);
-          idol.elementInCombination(null);
-          idol.moveDirection(null);
+
+      idol.readyToMove.subscribe(function(ready) {
+        if(ready) {
+          var departure = $(idol.moveFrom()).offset();
+          var destination = $(idol.moveTo()).offset();
+
+          var moveX = destination.left - departure.left;
+          var moveY = destination.top  - departure.top;
+          var distance = Math.sqrt(moveX * moveX + moveY * moveY);
+          var duration = Math.min(distance * 2, 500);
+
+          var ghost = $('<div class="ghost">').css({
+            position: 'fixed',
+            border: '1px solid darkgray',
+            transitionProperty: 'transform',
+            transitionDuration: duration + 'ms',
+            transitionTimingFunction: 'ease',
+            zIndex: 100,
+            width: $(idol.moveFrom()).outerWidth() + 'px',
+            height: $(idol.moveFrom()).outerHeight() + 'px',
+
+            // start state
+            top: departure.top,
+            left: departure.left,
+            transform: 'translate(0, 0)',
+          });
+
+          $(idol.moveFrom()).detach();
+
+          // show the new idol and remove the old one at end of animation
+          ghost.on('transitionend', function() {
+            console.log('transitionend');
+            ghost.detach();
+            idol.moveTo(null);
+            idol.moveFrom(null);
+          });
+
+          // start animation
+          ghost.appendTo('body');
+          ghost.get(0).clientHeight; // trigger rendering and transition on WebKit
+          ghost.css({
+            transform: 'translate(' + moveX + 'px,' + moveY + 'px)',
+          });
         }
       });
     });
 
-    this.addIdolToMenu = function(element, index, idol) {
-      if(element.nodeType === 1) {
-        idol.elementInMenu(element);
+    this.addIdol = function(element, index, idol) {
+      if(element.nodeType === Node.ELEMENT_NODE) {
+        idol.moveTo(element);
       }
     };
-    this.removeIdolFromMenu = function(element, index, idol) {
-      if(element.nodeType === 1) {
-        idol.elementInMenu(element);
-      }
-    };
-    this.addIdolToCombination = function(element, index, idol) {
-      if(element.nodeType === 1) {
-        idol.elementInCombination(element);
-        idol.moveDirection('Combination');
-      }
-    };
-    this.removeIdolFromCombination = function(element, index, idol) {
-      if(element.nodeType === 1) {
-        idol.elementInCombination(element);
-        idol.moveDirection('Menu');
+
+    this.removeIdol = function(element, index, idol) {
+      if(element.nodeType === Node.ELEMENT_NODE) {
+        idol.moveFrom(element);
+      } else {
+        $(element).detach();
       }
     };
   };
