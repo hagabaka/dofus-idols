dff38a7335bf0d0784422187bed2d0cb5c8ea607
Yaohan Chen <yaohan.chen@gmail.com>
2015-07-29 23:31:58 -0400

    Fix base path error

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 requirejs.config({
-  baseUrl: location.href,
+  baseUrl: location.href.replace(/[^\/]*$/, ''),
   paths: {
     jquery: 'https://code.jquery.com/jquery-1.11.3.min',
     knockout: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min',
