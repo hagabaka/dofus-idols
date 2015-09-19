4938175423943c70ed754f0ec9fec91439433f1e
Yaohan Chen <yaohan.chen@gmail.com>
2015-09-18 20:49:56 -0400

    Use protocol-relative urls and <link> for stylesheet

--- requirejs/config.js
+++ requirejs/config.js
@@ -1,9 +1,9 @@
 requirejs.config({
   baseUrl: location.href.replace(/[^\/]*$/, ''),
   paths: {
-    jquery: 'https://code.jquery.com/jquery-1.11.3.min',
-    knockout: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min',
-    underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.0/underscore-min',
+    jquery: '//code.jquery.com/jquery-1.11.3.min',
+    knockout: '//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min',
+    underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.0/underscore-min',
     text: 'requirejs/text',
     domReady: 'requirejs/domReady',
     thenBy: 'thenBy',
