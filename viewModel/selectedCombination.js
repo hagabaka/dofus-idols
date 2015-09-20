524b1bee2fa1e1b7c41ed482b03f4fdb61646a42
Yaohan Chen <yaohan.chen@gmail.com>
2015-09-20 13:17:24 -0400

    Display incompatibitilies of selected combo

--- viewModel/selectedCombination.js
+++ viewModel/selectedCombination.js
@@ -9,6 +9,26 @@ define([], function() {
         selectedCombination.synergies.push(synergy);
       });
     });
+    this.incompatiblities = ko.pureComputed(function() {
+      var result = {};
+      selectedCombination.idols().forEach(function(idol) {
+        idol.ineligible.forEach(function(dungeon) {
+          if(dungeon in result) {
+            result[dungeon].push(idol);
+          } else {
+            result[dungeon] = [idol];
+          }
+        });
+      });
+      var resultArray = [];
+      for(var dungeon in result) {
+        resultArray.push({
+          dungeon: dungeon,
+          idols: result[dungeon]
+        });
+      }
+      return resultArray;
+    });
     this.isFull = ko.pureComputed(function () {
       return selectedCombination.idols().length >= 6;
     });
